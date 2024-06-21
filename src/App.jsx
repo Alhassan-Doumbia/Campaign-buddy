b73df7e619bcc4ff131cf8a52f96484b63ea1f6a
Doumbia Alhassan <hassan4cod@gmail.com>
2024-06-21 17:56:20 +0000

    turned the navbar into a fully component for better customization

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import '../../Design-assets/Logo.png'
 import TextArea from './Components/textArea'
+import Navbar from './Components/Navbar'
 function App() {
   return (
     <>
       <section id="main-panel" className=" flex flex-col gap-3 px-12 size-full h-svh w-full border-2 border-green-400">
-        <header className='w-full h-16 flex items-center text-xl font-fugaz text-gray-800 '> Campaign Buddy </header>
+        {/* <header className='w-full h-16 flex items-center text-xl font-fugaz text-gray-800 '> Campaign Buddy </header> */}
+        <Navbar></Navbar>
         <div className='w-full h-fit min-h-[300px]  flex items-center justify-center'>
             <TextArea/>
         </div>
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <header className='w-full h-16 flex items-center text-xl font-fugaz text-gray-800 '> Campaign Buddy </header>
+  )
+}
+
+export default Navbar
